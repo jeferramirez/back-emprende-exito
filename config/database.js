9399b5f75388c897fe7ffd048748137427e61107
Miguel Castrillon <macs0330@hotmail.com>
2020-10-25 23:22:05 -0500

    creation of perfil_usuario table and fields are added in users

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'emprende_exito'),
-        username: env('DATABASE_USERNAME', 'jeff'),
+        username: env('DATABASE_USERNAME', 'emprende'),
         password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false),
       },
