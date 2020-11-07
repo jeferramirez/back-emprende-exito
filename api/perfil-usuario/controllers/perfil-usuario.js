b2fba22285936c9b025c08611ffa7e316da74c73
Miguel Castrillon <macs0330@hotmail.com>
2020-11-07 16:36:44 -0500

    creación de perfil_usuario

---
+++ api/perfil-usuario/controllers/perfil-usuario.js
@@ -0,0 +1,16 @@
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async perfilUser(ctx) {
+    const { idUser } = ctx.params;
+    return await strapi
+      .query('perfil-usuario')
+      .findOne({ users_permissions_user: idUser }, []);
+  },
+};
