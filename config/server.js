be78b4652b92ebacc577b99940667ad82a34a99c
Jeff <jefer.ramirezjrt@hotmail.com>
2020-11-08 14:53:19 -0500

    working

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
-    url: '/dashboard',
+
     auth: {
       secret: env('ADMIN_JWT_SECRET', '315149dc14528a4d9148b5133f232f56'),
     },
