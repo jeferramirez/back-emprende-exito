17bde180d7df00f4b94fc229e69469043c817552
Miguel Castrillon <macs0330@hotmail.com>
2020-11-09 20:41:20 -0500

    creacion de endpoint validatePass

--- api/perfil-usuario/controllers/perfil-usuario.js
+++ api/perfil-usuario/controllers/perfil-usuario.js
@@ -1,5 +1,5 @@
 "use strict";
-const { sanitizeEntity } = require("strapi-utils");
+const bcrypt = require("bcrypt");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
@@ -10,7 +10,22 @@ module.exports = {
   async perfilUser(ctx) {
     const { idUser } = ctx.params;
     return await strapi
-      .query('perfil-usuario')
+      .query("perfil-usuario")
       .findOne({ users_permissions_user: idUser }, []);
+  },
+
+  async validatePass(ctx) {
+    const { idUser, currentPass } = ctx.request.body;
+    const resp = await strapi
+      .query("perfil-usuario")
+      .findOne({ users_permissions_user: idUser }, ["users_permissions_user"]);
+    const hash = resp.users_permissions_user.password;
+
+    const result = await bcrypt.compare(currentPass, hash);
+    if (result) {
+      ctx.send({ code: 200, status: true });
+    } else {
+      ctx.send({ code: 400, status: false });
+    }
   },
 };
--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'emprende_exito'),
-        username: env('DATABASE_USERNAME', 'jeff'),
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: env.bool('DATABASE_SSL', false),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
       },
       options: {}
     },
