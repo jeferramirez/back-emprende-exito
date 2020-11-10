ff6fd7bfbeea23b7ae29a919cfd9cf6aaa8954c2
Miguel Castrillon <macs0330@hotmail.com>
2020-11-09 20:43:21 -0500

    se remueve el host

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST'),
         port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
