db8984cba64850796134bbd381cd891282fde5b5
Jeff <jefer.ramirezjrt@hotmail.com>
2020-11-29 14:17:46 -0500

    generate id recovery password user

--- api/ficha-seguimiento/controllers/ficha-seguimiento.js
+++ api/ficha-seguimiento/controllers/ficha-seguimiento.js
@@ -1,50 +1,64 @@
-'use strict';
+"use strict";
 
+const {uid} = require('uid')
 //
 module.exports = {
   async perfilUser(ctx) {
     const { idUser } = ctx.params;
     return await strapi
-    .query('ficha-seguimiento')
-    .find({ users_permissions_user: idUser }, ['users_permissions_user']);
-    },
+      .query("ficha-seguimiento")
+      .find({ users_permissions_user: idUser }, ["users_permissions_user"]);
+  },
 
-    async sendmail(ctx) {
+  async sendmail(ctx) {
+    const { userEmail } = ctx.request.body;
+    const user  = await strapi.query('user', 'users-permissions').findOne({ email: userEmail });
+    const bdEmail = user.email;
 
-      // validar que recibe email
-      const mailjet = require ('node-mailjet')
-      .connect('9601daa407b47b0c9f0b8632781a94da', 'c2bf5d2cc75c809e27cd0635a1f540a6')
-      const request = mailjet
-      .post("send", {'version': 'v3.1'})
-      .request({
-        "Messages":[
-          {
-            "From": {
-              "Email": "jefer.ramirezjrt@hotmail.com",
-              "Name": "jeff"
+    if (userEmail === bdEmail) {
+      const codeRecovery = uid();
+      const updateuser  = await strapi.query('user', 'users-permissions').update({ id: user.id }, { codeRecovery });
+
+        const mailjet = require("node-mailjet").connect(
+          "9601daa407b47b0c9f0b8632781a94da",
+          "c2bf5d2cc75c809e27cd0635a1f540a6"
+        );
+        const request = mailjet.post("send", { version: "v3.1" }).request({
+          Messages: [
+            {
+              From: {
+                Email: "jefer.ramirezjrt@hotmail.com",
+                Name: "jeff",
+              },
+              To: [
+                {
+                  Email: userEmail,
+                  Name: "jeff",
+                },
+              ],
+              Subject: "Reset password emprende con exito.",
+              TextPart: "usuario",
+              HTMLPart:
+                "<h3>Para resetear el password navegue al siguiente link <a href='https://www.mailjet.com/'>Emprende</a>!</h3><br />",
+              CustomID: "",
             },
-            "To": [
-              {
-                "Email": "macs0330@gmail.com",
-                "Name": "jeff"
-              }
-            ],
-            "Subject": "Greetings from Mailjet.",
-            "TextPart": "My first Mailjet email",
-            "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
-            "CustomID": "AppGettingStartedTest"
-          }
-        ]
-      })
-      request
-        .then((result) => {
-          console.log(result.body)
-        })
-        .catch((err) => {
-          console.log(err.statusCode)
-        })
+          ],
+        });
+        request
+          .then((result) => {
+            ctx.send({ code: 200, status: true });
+            console.log(result.body);
+          })
+          .catch((err) => {
+            ctx.send({ code: 400, status: false });
+            console.log(err.statusCode);
+          });
 
-    }
+      }
 
+    // validar que recibe email
+   /*
 
+    */
+  },
 };
