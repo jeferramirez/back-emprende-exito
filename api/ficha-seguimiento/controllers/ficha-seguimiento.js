23ef4ad153631dee12e4f0ba0248bcecd13baf7c
Miguel Castrillon <macs0330@hotmail.com>
2020-12-11 21:37:04 -0500

    Ajuste ficha_seguimiento

--- api/ficha-seguimiento/controllers/ficha-seguimiento.js
+++ api/ficha-seguimiento/controllers/ficha-seguimiento.js
@@ -1,60 +1,8 @@
-"use strict";
+'use strict';
 
-const {uid} = require('uid')
-//
-module.exports = {
-  async perfilUser(ctx) {
-    const { idUser } = ctx.params;
-    return await strapi
-      .query("ficha-seguimiento")
-      .find({ users_permissions_user: idUser }, ["users_permissions_user"]);
-  },
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
 
-  async sendmail(ctx) {
-    const { userEmail } = ctx.request.body;
-    const user  = await strapi.query('user', 'users-permissions').findOne({ email: userEmail });
-    const bdEmail = user.email;
-
-    if (userEmail === bdEmail) {
-      const codeRecovery = uid();
-      const url = 'http://localhost:4200/reset-pass/' + codeRecovery;
-      await strapi.query('user', 'users-permissions').update({ id: user.id }, { codeRecovery });
-
-        const mailjet = require("node-mailjet").connect(
-          "9601daa407b47b0c9f0b8632781a94da",
-          "c2bf5d2cc75c809e27cd0635a1f540a6"
-        );
-        const request = mailjet.post("send", { version: "v3.1" }).request({
-          Messages: [
-            {
-              From: {
-                Email: "jefer.ramirezjrt@hotmail.com",
-                Name: "jeff",
-              },
-              To: [
-                {
-                  Email: userEmail,
-                  Name: "jeff",
-                },
-              ],
-              Subject: "Reset password emprende con exito.",
-              TextPart: "usuario",
-              HTMLPart:
-              `<h3>Para resetear el password navegue al siguiente link <a href='${url}'>Emprende</a>!</h3><br />`,
-              CustomID: "",
-            },
-          ],
-        });
-
-        try {
-          const result =  await request;
-          console.log(result.body);
-          ctx.send({ code: 200, status: true });
-        } catch (error) {
-          ctx.send({ code: 400, status: false });
-          console.log(err.statusCode);
-        }
-
-      }
-  },
-};
+module.exports = {};
